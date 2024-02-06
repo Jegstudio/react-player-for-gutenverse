e2f2ced883acf6b8fbc1b115622be47bd98559ba
Agung Bayu <agungbayuiswara@gmail.com>
2024-02-06 10:31:35 +0800

    update react player

--- 2.14.1/lazy/utils.js
+++ 2.14.1/lazy/utils.js
@@ -188,5 +188,6 @@ function enableStubOn(fn) {
 }
 function getAssetPath() {
   const { framework_asset } = window.GutenverseFrontendConfig;
+  console.log({ framework_asset });
   return framework_asset + "js";
 }
